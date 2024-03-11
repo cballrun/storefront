86b1be73b2382fb0a6585d410e8297ffa2fa368e
cballrun <carter.k.ball@gmail.com>
2024-03-11 15:57:07 -0400

    Add dictionary containing name : Poophead key value pair and dynamically render it within hello.html

--- playground/views.py
+++ playground/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    return render(request, 'hello.html', { 'name' : 'Poophead'} )
     
